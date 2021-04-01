2188b3c45ff3f332f0d63273b73daf8411acdcbf
Aleksandr Zaytsev <zaytsev.a@pharm-portal.ru>
2021-04-01 11:41:04 +0300

    Fix dock apps

--- osx/dock.sh
+++ osx/dock.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/Calendar.app"
-dockutil --no-restart --add "/Applications/Utilities/Activity Monitor.app"
-dockutil --no-restart --add "/Applications/System Preferences.app"
+dockutil --no-restart --add "/System/Applications/Calendar.app"
+dockutil --no-restart --add "/System/Applications/Utilities/Activity Monitor.app"
+dockutil --no-restart --add "/System/Applications/System Preferences.app"
 
 killall Dock
